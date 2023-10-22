551e4f67ac45565100fb9f5f1de6c485bd9b5274
Mick210 <sid222m@gmail.com>
2023-10-22 09:28:56 +0300

    Initial commit

---
+++ com/example/hw_2_9/Hw29Application.java
@@ -0,0 +1,13 @@
+package com.example.hw_2_9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw29Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw29Application.class, args);
+	}
+
+}
